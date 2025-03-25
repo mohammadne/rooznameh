2f25e6fd1ae2c56492d503698c9d98913b3b8ae3
mohammadne <mohammadne.dev@gmail.com>
2025-03-25 12:09:42 +0330

    chore: update post view

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts_list.html', {'posts': posts})
